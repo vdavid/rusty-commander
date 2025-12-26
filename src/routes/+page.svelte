916eeec9d265215d5f35dfcaa06eab27973b85fe
David Veszelovszki <veszelovszki@gmail.com>
2025-12-26 19:25:12 +0100

    Move CSS to app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,30 +30,3 @@
 </script>
 
 <DualPaneExplorer />
-
-<style>
-    :global(html, body) {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        background-color: var(--color-bg-primary);
-        color: var(--color-text-primary);
-        /* Disable text selection globally */
-        user-select: none;
-        -webkit-user-select: none;
-        /* Force default cursor everywhere */
-        cursor: default;
-    }
-
-    :global(#app) {
-        width: 100%;
-        height: 100%;
-    }
-
-    /* Disable scroll bounce on all scrollable elements */
-    :global(*) {
-        overscroll-behavior: none;
-    }
-</style>
